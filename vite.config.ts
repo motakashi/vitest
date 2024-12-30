f41bf7c6a273f8ab0de52c028259ff5ee9cc8ab7
motakashi <takashi-going-sky@hotmail.co.jp>
2024-12-31 04:15:00 +0900

    設定の些細な変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
-/// <reference types="vitest"/>
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import tsconfigPaths from "vite-tsconfig-paths";
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  // happy-dom用の設定
   test: {
-    environment: "happy-dom",
-    setupFiles: ["./vitest-setup.ts"]
+    environment: 'happy-dom',
+    setupFiles: ['./vitest-setup.ts'],
   },
-})
-
+});
